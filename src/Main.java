af60bef7bd753cb39afd1a6d50020701a1a7342f
carwynteifion <carwynteifion@yahoo.co.uk>
2023-12-18 16:39:13 +0000

    Add additional comments

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,16 @@ public class Main {
         // Creates a new scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
-        // Prints welcome message
+        // Prints welcome message to the user
         System.out.println("Welcome to USW Employee Management System");
 
-        // Prompts user to input full name
+        // Prompts user to input full name and assigns input to a new String named fullName
         System.out.println("Please enter your full name:");
         String fullName = scanner.nextLine();
 
         /* Ensures fullName user input only consists of letters and spaces, is not an empty string,
-        and contains a space which is not at the start or end of the string, before proceeding. */
+        and contains a space which is not at the start or end of the string, before proceeding.
+        Prints appropriate error messages if any of the above is true. */
         while (
                 !fullName.matches("^[-'a-zA-ZÀ-ÿ\\s]*$") ||
                 !fullName.contains(" ") ||
@@ -33,7 +34,8 @@ public class Main {
             fullName = scanner.nextLine();
         }
 
-        // Input age; also checks value entered is an int that is between 18 and 100.
+        /* Prompts user to input age; also checks value entered is an int between 18 and 100.
+        If the value is an int, assigns user input to a new int, employeeAge. */
         int employeeAge;
         do {
             System.out.println("Please enter your age (18-100):");
@@ -50,7 +52,8 @@ public class Main {
             }
         } while (true);
 
-        // Input employee number; also checks value entered is an int
+        /* Prompts user to input employee number; also checks value entered is an int.
+        If the value is an int, assigns user input to a new int, employeeNumber. */
         int employeeNumber;
         System.out.println("Please enter your employee number:");
         while (!scanner.hasNextInt()) {
@@ -59,20 +62,36 @@ public class Main {
         }
         employeeNumber = scanner.nextInt();
 
-        // Ensures employee number is displayed as eight characters
+        /* Ensures employee number is displayed as eight characters using string formatting.
+        The new String, eightDigitEmployeeNumber, is assigned the value of the String
+        employeeNumber, formatted to eight digits by passing it through the function
+        String.format().
+         */
         String eightDigitEmployeeNumber = String.format("%08d", employeeNumber);
 
-        // Calculate pseudo-random PIN.
+        // Calculates the pseudo-random PIN.
+        // First, displays a message to the user to confirm the PIN is being generated.
         System.out.println("Generating PIN...");
+
+        /* Then, creates a new string array, nameSplit, which is assigned the value of
+        fullName after being split into array items wherever there is a space, using
+        fullName.split(" ").*/
         String[] nameSplit = fullName.split(" ");
+
+        // A new string, firstName, is assigned the value of the array's first item.
         String firstName = nameSplit[0];
+
+        // Then, the int pin is calculated by multiplying the length of firstName by employeeAge.
         int pin = firstName.length() * employeeAge;
+
+        /* Finally, to ensure the pin is always at most four digits, the line pin %= 10000
+        returns the remainder of the pin's value when divided by 10000. */
         pin %= 10000;
 
-        // Convert calculated PIN to 4 digit string
+        // Convert calculated PIN to 4 digit string using string formatting
         String fourDigitPin = String.format("%04d", pin);
 
-        //Provide PIN to user
+        // Confirms user inputs and account creation, and provides PIN to user
         System.out.println(
                 "Hi " + fullName + ". Your employee account "
                         + eightDigitEmployeeNumber + " has been created. Your unique PIN is " + fourDigitPin + "."
